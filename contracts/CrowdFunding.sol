f80b088d2d6af751a1f142be18ea2a530e8928f9
Devarsh Chandiwade <119281406+IMxSPYDER@users.noreply.github.com>
2025-02-22 07:01:31 +0530

    Add files via upload

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string state;
+        string region;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image, string memory _state, string memory _region) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+        campaign.state = _state;
+        campaign.region = _region;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
+
+    function getCampaignsByOwner(address _owner) public view returns (Campaign[] memory) {
+    uint count = 0;
+
+    // Count the number of campaigns owned by the user
+    for (uint i = 0; i < numberOfCampaigns; i++) {
+        if (campaigns[i].owner == _owner) {
+            count++;
+        }
+    }
+
+    // Create a new array to store user's campaigns
+    Campaign[] memory userCampaigns = new Campaign[](count);
+    uint index = 0;
+
+    for (uint i = 0; i < numberOfCampaigns; i++) {
+        if (campaigns[i].owner == _owner) {
+            userCampaigns[index] = campaigns[i];
+            index++;
+        }
+    }
+
+    return userCampaigns;
+}
+}
